a8d019c0627e0b1c005875608d3de06d8edd6c6b
ManhTTFU <tuanmanh97x@gmail.com>
2025-09-05 22:38:20 +0700

    first commit

---
+++ src/main/java/org/example/crud/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package org.example.crud.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
