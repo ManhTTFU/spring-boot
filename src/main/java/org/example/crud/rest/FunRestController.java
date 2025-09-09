f64f15dc167cc8411edadd25a00c8644464a950f
ManhTTFU <tuanmanh97x@gmail.com>
2025-09-09 21:37:16 +0700

    injecting custom application properties

--- src/main/java/org/example/crud/rest/FunRestController.java
+++ src/main/java/org/example/crud/rest/FunRestController.java
@@ -1,12 +1,34 @@
 package org.example.crud.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/team")
+    public String getCoachName() {
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        return "Hello World 3";
+    }
+
+    @GetMapping("/milk")
+    public String milk() {
+        return "milk";
+    }
+    @GetMapping("/chicken")
+    public String chicken() {
+        return "chicken";
     }
 }
