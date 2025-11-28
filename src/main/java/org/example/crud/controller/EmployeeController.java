b3adf2241fb5b5b0b3337d619d2e1725af8649bc
ManhTTFU <tuanmanh97x@gmail.com>
2025-11-28 17:05:41 +0700

    add thymleaf

---
+++ src/main/java/org/example/crud/controller/EmployeeController.java
@@ -0,0 +1,44 @@
+package org.example.crud.controller;
+
+import org.example.crud.entity.Employee;
+import org.example.crud.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        List<Employee> employees = employeeService.findAll();
+        model.addAttribute("employees", employees);
+        return "list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save the employee
+        employeeService.save(employee);
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+}
--- src/main/java/org/example/crud/security/DemoSecurityConfig.java
+++ src/main/java/org/example/crud/security/DemoSecurityConfig.java
@@ -2,15 +2,15 @@ package org.example.crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.provisioning.JdbcUserDetailsManager;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -19,41 +19,42 @@ public class DemoSecurityConfig {
 
     // add support for jdbc ... no more hardcode users
 
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
-
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
-        return jdbcUserDetailsManager;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                    .requestMatchers(
-                            "/v3/api-docs/**",
-                            "/swagger-ui/**",
-                            "/swagger-ui.html"
-                    ).permitAll()
-                    .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
-                    .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
-                    .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
-                    .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
-                    .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
-        );
-        // use http basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-        // disable Cross site request forgery (CSFR)
-        // in general, not required for stateless REST APTs that uss POST, PUT, DELETE and/or PATCH
-
-        http.csrf(csrf -> csrf.disable());
-        return http.build();
-    }
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//
+//        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+//
+//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+//        return jdbcUserDetailsManager;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.authorizeHttpRequests(configurer ->
+//                configurer
+//                    .requestMatchers(
+//                            "/v3/api-docs/**",
+//                            "/swagger-ui/**",
+//                            "/swagger-ui.html"
+//                    ).permitAll()
+//                    .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+//                    .requestMatchers(HttpMethod.GET, "employees/list").hasRole("EMPLOYEE")
+//                    .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+//                    .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+//                    .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+//                    .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+//        );
+//        // use http basic authentication
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        // disable Cross site request forgery (CSFR)
+//        // in general, not required for stateless REST APTs that uss POST, PUT, DELETE and/or PATCH
+//
+//        http.csrf(csrf -> csrf.disable());
+//        return http.build();
+//    }
 
 //    @Bean
 //    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
